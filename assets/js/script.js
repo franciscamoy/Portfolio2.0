d28814bd76f115ded843c7a0dfb36a63871b6bff
franciscamoy <60185975+franciscamoy@users.noreply.github.com>
2020-03-31 14:55:51 -0300

    last commit 31_03

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,5 @@
-/*$(document).ready(function() {
 
-	$('.link-gallery').click(function(){
-		var descripcion = $(this).attr('title');
-		$('#caption').html(descripcion);
-	  	var img = $(this).find('img');
-	  	var src = img.attr('src')
-	  	$('#img01').attr('src', src);
-		$('#myModal').css('display','block');
-		$('.modal-backdrop').remove();
-	});
 
-	$('.close').click(function(){
-		$('#myModal').css('display','none');
-	});
-	
-}); */
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
